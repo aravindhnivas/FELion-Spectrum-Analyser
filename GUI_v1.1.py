5e7e5cb60465320c2cac5c1b2e4135d8e8a58257
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-11-25 16:45:03 +0100

    Create GUI_v1.1.py

---
+++ GUI_v1.1.py
@@ -0,0 +1,37 @@
+## A simple GUI program for FELion Instrument:
+
+from tkinter import *
+from tkinter import messagebox
+
+# User defined definitions:
+def on_closing():
+  if messagebox.askokcancel("Quit", "Do you want to quit?"):
+    root.destroy()
+
+#Creating a class and also inheriting another class Tk() from tkinter
+class FELion(Tk):
+
+  def __init__(self, *args, **kwargs):
+    #First let us take the initialsing parameters from the inherited class Tk() from tkinter:
+    Tk.__init__(self, *args, **kwargs)
+    pass
+
+root = Tk()
+#Defining the main window's dimensions:
+width_window = 700
+height_window = 500
+#Making window appear at the center of your computer screen:
+your_computer_screen_width = root.winfo_screenwidth()
+your_computer_screen_height = root.winfo_screenheight()
+x_coordinate = (your_computer_screen_width/2) - (width_window/2) 
+y_coordinate = (your_computer_screen_height/2) - (height_window/2)
+root.geometry("%dx%d+%d+%d" %(width_window, height_window, x_coordinate, y_coordinate))
+#Defining the window titile
+root.title("FELion Spectrum Analyser")
+#Configuring window
+root.config()
+
+
+######################## TOP ########################
+
+root.mainloop()
