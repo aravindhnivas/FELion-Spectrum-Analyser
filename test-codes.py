d1847c6b03e8b04b467d1e407018d3e39789907c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-11-27 15:06:17 +0100

    test

---
+++ test-codes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from tkinter import *
+from FELion_avgSpec import avgSpec_plot
+from FELion_baseline import baseline_correction
+from FELion_normline import normline_correction
+
+root = Tk()
+root.geometry("500x500")
+
+topFrame = Frame(root, height = 50)
+topFrame.pack(fill = "both", expand = False)
+
+
+frame = Frame(root, bg = "sea green")
+frame.pack(fill = "both", expand = True)
+
+content = StringVar()
+user_input = Entry(frame, textvariable = content)
+content.set("Enter")
+user_input.focus()
+user_input.pack(pady = 10)
+
+
+avg = Button(frame, text = "avg", command = lambda: avgSpec_plot())
+avg.pack(pady = 10)
+
+norm = Button(frame, text = "norm", command = lambda: normline_correction(user_input.get()))
+norm.pack(pady = 10)
+
+root.mainloop()
