5517f02a4898607d8603f01e084b19838cad246d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-11-30 18:18:03 +0100

    Update test_code2.py

--- test_code2.py
+++ test_code2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 from tkinter import *
-'''from FELion_avgSpec import avgSpec_plot
+from FELion_avgSpec import avgSpec_plot
 from FELion_normline import normline_correction
-from f_baseline import f_baseline_correction'''
+from f_baseline import f_baseline_correction
 
-
-import FELion_avgSpec, f_baseline, FELion_normline
 
 root = Tk()
 root.geometry("500x500")
@@ -23,12 +21,12 @@ content.set("Enter")
 user_input.focus()
 user_input.pack(pady = 10)
 
-avg = Button(frame, text="avg", command = lambda: FELion_avgSpec.avgSpec_plot())
+avg = Button(frame, text="avg", command = lambda: avgSpec_plot())
 avg.pack(pady = 10)
 
-norm = Button(frame, text="norm", command = lambda: FELion_normline.normline_correction(user_input.get()))
+norm = Button(frame, text="norm", command = lambda: normline_correction(user_input.get()))
 norm.pack(pady = 10)
 
-base = Button(frame, text = "base", command = lambda: f_baseline.f_baseline_correction(user_input.get()))
+base = Button(frame, text = "base", command = lambda: f_baseline_correction(user_input.get()))
 base.pack(pady = 10)
 root.mainloop()
