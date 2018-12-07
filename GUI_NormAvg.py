f4a8e8ef23c2a703559e123551e326fffa74ecc4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-07 01:32:36 +0100

    Update GUI_NormAvg.py

--- GUI_NormAvg.py
+++ GUI_NormAvg.py
@@ -8,6 +8,10 @@ from tkinter import ttk
 from FELion_normline import normline_correction
 from FELion_avgSpec import avgSpec_plot
 
+
+def avg_buttons():
+    pass
+
 def gui_normline():
     # User defined definitions:
     ###########################################################################################
@@ -19,8 +23,8 @@ def gui_normline():
     root = Tk()
 
     #Defining the main window's dimensions:
-    width_window = 600
-    height_window = 400
+    width_window = 700
+    height_window = 700
     root.geometry("{}x{}".format(width_window, height_window))
     root.title("FELion Spectrum Analyser")
 
@@ -90,17 +94,85 @@ def gui_normline():
     normline_button = ttk.Button(bottomFrame, text="Normline")
     normline_button.config(command = lambda: normline_correction(content.get()))
 
+    
+
+    # Avg_Spectrum Labels:
+    avg_title = Label(bottomFrame, text = "Title:", font=("Times", 10, "bold"))
+    avg_ts = Label(bottomFrame, text = "Title\nsize:", font=("Times", 10, "bold"))
+    avg_lgs = Label(bottomFrame, text = "Legend\nSize:", font=("Times", 10, "bold"))
+    avg_minor = Label(bottomFrame, text = "Minor\naxis:", font=("Times", 10, "bold"))
+    avg_major = Label(bottomFrame, text = "Major\naxis:", font=("Times", 10, "bold"))
+    avg_majorTick = Label(bottomFrame, text = "Major\nTickSz:", font=("Times", 10, "bold"))
+    avg_xmin = Label(bottomFrame, text = "X-min:", font=("Times", 10, "bold"))
+    avg_xmax = Label(bottomFrame, text = "X-max:", font=("Times", 10, "bold"))
+
+    # avg_label's Entry widget:
+    i_avg_title = StringVar()
+    i_avg_ts = IntVar() 
+    i_avg_lgs = IntVar() 
+    i_avg_minor = IntVar() 
+    i_avg_major = IntVar() 
+    i_avg_majorTick = IntVar() 
+    i_avg_xmin = IntVar() 
+    i_avg_xmax = IntVar()
+        
+    i_avg_title.set("Title")
+    i_avg_ts.set(10)
+    i_avg_lgs.set(5)
+    i_avg_minor.set(5)
+    i_avg_major.set(50)
+    i_avg_majorTick.set(8)
+    i_avg_xmin.set(1000)
+    i_avg_xmax.set(2000)
+
+    avg_title_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_title, justify = LEFT, font=("Times", 10, "bold"))
+    avg_ts_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_ts, justify = LEFT, font=("Times", 10, "bold"))
+    avg_lgs_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_lgs, justify = LEFT, font=("Times", 10, "bold"))
+    avg_minor_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_minor, justify = LEFT, font=("Times", 10, "bold"))
+    avg_major_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_major, justify = LEFT, font=("Times", 10, "bold"))
+    avg_majorTick_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_majorTick, justify = LEFT, font=("Times", 10, "bold"))
+    avg_xmin_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_xmin, justify = LEFT, font=("Times", 10, "bold"))
+    avg_xmax_Entry = Entry(bottomFrame, bg = "white", bd = 5, textvariable=i_avg_xmax, justify = LEFT, font=("Times", 10, "bold"))
+    
+    # Placing the avg_labels and Entry widgets:
+    avg_title.place(relx = 0.5,  rely = 0.1, width = 100, height = 40)
+    avg_ts.place(relx = 0.5,  rely = 0.2, width = 100, height = 40)
+    avg_lgs.place(relx = 0.5,  rely = 0.3, width = 100, height = 40)
+    avg_minor.place(relx = 0.5,  rely = 0.4, width = 100, height = 40)
+    avg_major.place(relx = 0.5,  rely = 0.5, width = 100, height = 40)
+    avg_majorTick.place(relx = 0.5,  rely = 0.6, width = 100, height = 40)
+    avg_xmin.place(relx = 0.5,  rely = 0.7, width = 100, height = 40)
+    avg_xmax.place(relx = 0.5,  rely = 0.8, width = 100, height = 40)
+
+    avg_title_Entry.place(relx = 0.7,  rely = 0.1, width = 100, height = 40)
+    avg_ts_Entry.place(relx = 0.7,  rely = 0.2, width = 100, height = 40)
+    avg_lgs_Entry.place(relx = 0.7,  rely = 0.3, width = 100, height = 40)
+    avg_minor_Entry.place(relx = 0.7,  rely = 0.4, width = 100, height = 40)
+    avg_major_Entry.place(relx = 0.7,  rely = 0.5, width = 100, height = 40)
+    avg_majorTick_Entry.place(relx = 0.7,  rely = 0.6, width = 100, height = 40)
+    avg_xmin_Entry.place(relx = 0.7,  rely = 0.7, width = 100, height = 40)
+    avg_xmax_Entry.place(relx = 0.7,  rely = 0.8, width = 100, height = 40)
+
     #Avg_Spectrum
     avg_button = ttk.Button(bottomFrame, text="Avg_spectrum")
-    avg_button.config(command = lambda: avgSpec_plot())
-
+    avg_button.config(command = lambda: avgSpec_plot(i_avg_title.get(), \
+                                                            i_avg_ts.get(), \
+                                                            i_avg_lgs.get(), \
+                                                            i_avg_minor.get(), \
+                                                            i_avg_major.get(), \
+                                                            i_avg_majorTick.get(), \
+                                                            i_avg_xmin.get(), \
+                                                            i_avg_xmax.get())
+                                                            )
 
     #Placing the labels and buttons in bottom frame using place(), relx/y is relative to parent frame pixels
     user_input_label.place(relx = 0.1,  rely = 0.1, width = 100, height = 40)
     user_input.place(relx = 0.3,  rely = 0.1, width = 100, height = 40)
-    normline_button.place(relx = 0.3,  rely = 0.3, width = 100, height = 40)
-    avg_button.place(relx = 0.5,  rely = 0.3, width = 100, height = 40)
-    quitButton.place(relx = 0.3,  rely = 0.7, width = 100, height = 40)
+    normline_button.place(relx = 0.1,  rely = 0.3, width = 100, height = 40)
+    avg_button.place(relx = 0.3,  rely = 0.3, width = 100, height = 40)
+    quitButton.place(relx = 0.2,  rely = 0.9, width = 100, height = 40)
+
+    
 
     ###########################################################################################
     ###########################################################################################
