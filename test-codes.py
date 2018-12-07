cc1025a0cebf1ab329c9648c8dca123168157816
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-07 21:21:51 +0100

    testing

--- test-codes.py
+++ test-codes.py
@@ -1,25 +1,63 @@
- mass_fileList = []
+from tkinter import *
 
-        def popupinput():
-            pop = Tk()
-            pop.geometry("400x400")
+#Defining root frame window
+root = Tk()
 
-            pop_label = Label(pop, text = "Enter the mass_spec file names (comma separated): ", font=("Times", 10, "bold"))
-            pop_label.pack()
-            
-            pop_input = StringVar()
+# Defining frames
+topFrame = Frame(root, bg = "white", width=300, height=200)
+bottomFrame = Frame(root, bg = "green")
+label = Label(topFrame,\
+              text = "FELion Spectrum Analyser",\
+              bg = "green",\
+              width=30, height=1,\
+              font=("Courier", 30))
 
-            pop_entry = Entry(pop, bg = "white", bd = 5, textvariable = pop_input, justify = LEFT, font=("Times", 10, "bold"))
-            pop_input.set("Enter here")
-            pop_entry.focus_set()
-            pop_entry.pack()
+#Packing franes
+topFrame.pack(fill = X)
+bottomFrame.pack(side = BOTTOM,fill=X)
+label.pack()
 
-            done_button = ttk.Button(pop, text = "DONE", command = lambda: pop.quit())
-            done_button.pack()
+#Defining buttons:
 
-            fileList = pop_input.get()
-            pop.mainloop()
-            
-            return fileList.split(",")
-        
-        display_label = Label(bottomFrame, font=("Times", 10, "italic"))
+#Defining input file name:
+def retrieve_input():
+    inputValue=textBox.get()
+    file = str(inputValue)
+    if file == "":
+        print()
+        print("###############################################################")
+        print("Please enter the file name in the white box and click submit.")
+        print("###############################################################")
+    else:
+        print()
+        print("###############################################################")
+        print("The file name is: {}".format(file))
+        print("###############################################################")
+    #return file
+
+buttonInput=Button(bottomFrame,\
+                   height=1, width=10,\
+                   text="Submit",\
+                   command = lambda: retrieve_input())
+buttonInput.pack(side = RIGHT, padx=2, pady=2)
+textBox=Entry(bottomFrame)
+textBox.pack()
+
+#Baseline
+baseline_button = Button(bottomFrame, text="Baseline", width=20, height=1)
+baseline_button.bind("<Button-1>",lambda: print("Hello"))
+baseline_button.pack(side = TOP, padx=2, pady=2)
+
+#Normline
+normline_button = Button(bottomFrame, text="Normline", width=20, height=1)
+normline_button.bind("<Button-1>", lambda: print("Hello"))
+normline_button.pack(side = TOP, padx=2, pady=2)
+
+#Avg_Spectrum
+avg_button = Button(bottomFrame, text="Avg_spectrum", width=20, height=1)
+avg_button.bind("<Button-1>", lambda: print("Hello"))
+avg_button.pack(side = TOP, padx=2, pady=2)
+
+
+#Creating a stable window to stat untill closed
+root.mainloop()
