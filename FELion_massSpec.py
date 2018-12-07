f79db54cd858ac692701172581b0500050a6aed0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-07 17:36:03 +0100

    Update FELion_massSpec.py

--- FELion_massSpec.py
+++ FELion_massSpec.py
@@ -83,7 +83,7 @@ def main(date):
 
         plt.show()
 
-def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, fig_width, fig_height):
+def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, fig_width, fig_height, nfiles):
 
     plt.rcParams['figure.figsize'] = [fig_width,fig_height]
     plt.rcParams['figure.dpi'] = 80
@@ -126,3 +126,41 @@ def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, fig_width, fig_height):
     plt.title("Filename: {}, for {}, at temp: {}K, B0: {}ms and IE(eV): {}".format(fname, mname, temp, bwidth, ie))
     plt.savefig(my_path + r"\MassSpec_DATA\{}.png".format(fname))
     plt.show()
+
+    combine = False
+    if combine:
+        for scans in range(nfiles):
+            #date = input('scan number or filename: ')
+            if len(cfile) <2:
+                cfile= fname[0:9] + cfile +'.mass'
+            else:
+                date= date + '.mass'
+            with open(date) as f:
+                data = f.read()
+
+            data = data.split('\n')
+
+            x = -np.ones(len(data))
+            y = -np.ones(len(data))
+
+            for index, a in enumerate(data):
+
+                if '#' in a or len(a) < 1:
+                    if '# m03_ao09_width' in a:
+                        a = a.split('\t\t#')
+                        b0 = "%.0f" % (int(a[3]) / 1000)
+                else:
+                    a = a.split('\t')
+                    x[index] = float(a[0])
+                    y[index] = float(a[1])
+
+            name = f.name[:8].replace('_', '-')
+
+            plt.semilogy(x, y, label= input('legenda input: '))
+            plt.legend()
+
+        plt.xlabel('mass [u]')
+        plt.ylabel('ion counts /{} ms'.format(b0))
+        plt.grid(True)
+        plt.ylim(1)
+        plt.show()
