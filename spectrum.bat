98612123d45bb8b30038f0e51d8a8f54b0583b2b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-07 12:35:04 +0100

    exe

--- spectrum.bat
+++ spectrum.bat
@@ -1,6 +1,6 @@
 @echo off
 set root=C:\ProgramData\Anaconda3
-set file=D:\Work\RU\pythonScripts\FELion-Spectrum-Analyser
+set file=X:\NewPythonScripts_FELion\FELion-Spectrum-Analyser
 call %root%\Scripts\activate.bat %root%
 call pushd %cd%
 start python %file%\GUI_Baseline.py
