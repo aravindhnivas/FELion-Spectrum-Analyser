a58a66ce4651948d4bc527bcf735aed72b32ada5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-11 21:50:37 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,42 @@
-REM It just copy all the files in this directory to a location
-REM and add that path into the system variable:
-
 @echo off
-ROBOCOPY .\modules C:\FELion-GUI
+
+:: BatchGotAdmin
+:-------------------------------------
+::  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+:: --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:-------------------------------------- 
+
+ 
+ROBOCOPY %cd%\modules C:\FELion-GUI
 IF EXIST C:\FELion-GUI set PATH = %PATH%;C:\FELion-GUI
 
-REM ####################
+:: ####################
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-REM echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Normline.lnk" >> %SCRIPT%
+:: echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Normline.lnk" >> %SCRIPT%
 echo sLinkFile = "C:\FELion-GUI\FELion-Normline.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "C:\FELion-GUI\Normline.bat" >> %SCRIPT%
@@ -17,10 +45,10 @@ echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
 
-REM ####################
+:: ####################
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-REM echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Baseline.lnk" >> %SCRIPT%
+:: echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Baseline.lnk" >> %SCRIPT%
 echo sLinkFile = "C:\FELion-GUI\FELion-Baseline.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "C:\FELion-GUI\Baseline.bat" >> %SCRIPT%
@@ -29,12 +57,12 @@ echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
 
-REM ####################
+:: ####################
 
-REM ####################
+:: ####################
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-REM echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Powerfile.lnk" >> %SCRIPT%
+:: echo sLinkFile = "%USERPROFILE%\Desktop\FELion-Powerfile.lnk" >> %SCRIPT%
 echo sLinkFile = "C:\FELion-GUI\FELion-Powerfile.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "C:\FELion-GUI\Powerfile.bat" >> %SCRIPT%
@@ -43,25 +71,14 @@ echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
 
-REM ####################
+:: ####################
 
 
-copy C:\FELion-GUI\FELion-Normline.lnk %userprofile%\Desktop
-copy C:\FELion-GUI\FELion-Baseline.lnk %userprofile%\Desktop
-copy C:\FELion-GUI\FELion-Powerfile.lnk %userprofile%\Desktop
-
-copy C:\FELion-GUI\FELion-Normline.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-copy C:\FELion-GUI\FELion-Baseline.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-copy C:\FELion-GUI\FELion-Powerfile.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-
-REM ####################
+ROBOCOPY C:\FELion-GUI\ "%userprofile%\Desktop" FELion-Normline.lnk FELion-Baseline.lnk FELion-Powerfile.lnk
 
 
-REM pushd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-REM mkdir FELion
-REM copy C:\FELion-GUI\FELion-Normline.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\FELion"
-REM copy C:\FELion-GUI\FELion-Baseline.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\FELion"
-REM copy C:\FELion-GUI\FELion-Powerfile.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\FELion"
+ROBOCOPY C:\FELion-GUI "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" FELion-Normline.lnk FELion-Baseline.lnk FELion-Powerfile.lnk
+
 
 
 pause
