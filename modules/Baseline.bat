0bffec5f04a26386cd445a679625ded18d73b148
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2018-12-17 12:23:35 +0100

    update

--- modules/Baseline.bat
+++ modules/Baseline.bat
@@ -1,8 +1,6 @@
 @echo off
 
-REM set root=C:\ProgramData\Anaconda3
 set file=C:\FELion-GUI
-REM call %root%\Scripts\activate.bat %root%
 call activate base
 call python %file%\FELion_GUI_Baseline_v2.py
 
--- modules/Normline.bat
+++ modules/Normline.bat
@@ -1,8 +1,6 @@
 @echo off
 
-REM set root=C:\ProgramData\Anaconda3
 set file=C:\FELion-GUI
-REM call %root%\Scripts\activate.bat %root%
 call activate base
 call python %file%\FELion_GUI_v2.py
 
