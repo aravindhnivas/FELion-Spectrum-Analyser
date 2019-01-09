5a9d03f9d8c4b56ff1eef6a89ab2b541980500c0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-01-09 23:04:32 +0100

    Update FELion_massSpec.py

--- modules/FELion_massSpec.py
+++ modules/FELion_massSpec.py
@@ -87,7 +87,7 @@ def main(date):
 
         plt.show()
 
-def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, location,\
+def massSpec1(fname, mname, temp, bwidth, ie, xmin, xmax, location,\
             fig_width, fig_height, filelist, avgname, combine, save_fig):
 
     os.chdir(location)
@@ -208,3 +208,89 @@ def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, location,\
             
     except:
         filenotfound()
+
+def massSpec(fname, mname, temp, bwidth, ie, xmin, xmax, location,\
+            fig_width, fig_height, filelist, avgname, combine, save_fig):
+
+    os.chdir(location)
+    my_path = os.getcwd()
+    if fname.find(".mass")>0:
+        fname = fname.split(".")[0]
+
+    def saveinfo(name):
+        if os.path.isfile(my_path+"/MassSpec_DATA/{}.png".format(name)):
+            root = Tk()
+            root.withdraw()
+            messagebox.showinfo("Information", "File '{}.png' SAVED \nin MassSpec_DATA directory"\
+                                .format(name))
+            root.destroy()
+
+    if not combine:
+        
+        filename = fname + ".mass"
+
+        if not os.path.isdir("MassSpec_DATA"):
+            os.mkdir("MassSpec_DATA")
+            shutil.copyfile(my_path + "/{}".format(filename), my_path + "/MassSpec_DATA/{}".format(filename))
+        else:
+            shutil.copyfile(my_path + "/{}".format(filename), my_path + "/MassSpec_DATA/{}".format(filename))
+
+        f = open(filename)
+        x, y = [],[]
+        for i in f:
+            if not i[0] == "#" and not i == "\n":
+                a, b = i.split()
+                x.append(float(a))
+                y.append(float(b))
+        f.close()
+        
+        plt.grid(True)
+        plt.semilogy(x, y)
+        plt.xlabel('Mass [u]')
+        plt.ylabel('Ion counts /{} ms'.format(bwidth))
+        plt.title("Filename: {}, for {}, at temp: {}K, B0: {}ms and IE(eV): {}"\
+                    .format(fname, mname, temp, bwidth, ie))
+        if save_fig:
+            plt.savefig(my_path + "/MassSpec_DATA/{}.png".format(fname))
+            plt.show()
+            saveinfo(fname)
+        else:
+            plt.show()
+
+    if combine:
+        filelist = filelist.split(",")
+
+        for file in filelist:
+            file = file.strip()
+            if file.find("mass")>=0:
+                file = file.split(".")[0]
+                
+            if len(file) <2:
+                file= fname[0:9] + file +'.mass'
+            else:
+                file = file + '.mass'
+            f = open(filename)
+            x, y = [],[]
+            for i in f:
+                if not i[0] == "#" and not i == "\n":
+                    a, b = i.split()
+                    x.append(float(a))
+                    y.append(float(b))
+            f.close()
+            
+            plt.grid(True)
+            plt.semilogy(x, y, label = file)
+        
+            plt.legend()
+
+        plt.xlabel('mass [u]')
+        plt.ylabel('ion counts /{} ms'.format(bwidth))
+        plt.grid(True)
+        plt.title("Filename: {}, for {}, at temp: {}K, B0: {}ms and IE(eV): {}".format(fname, mname, temp, bwidth, ie))
+        
+        if save_fig:
+            plt.savefig(my_path + "/MassSpec_DATA/{}.png".format(avgname))
+            plt.show()
+            saveinfo(avgname)
+        else:
+            plt.show()
