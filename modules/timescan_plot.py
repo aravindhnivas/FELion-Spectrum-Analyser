6980334f274c45438f2869ee33d723fca66812c5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-01-11 20:38:42 +0100

    Update timescan_plot.py

--- modules/timescan_plot.py
+++ modules/timescan_plot.py
@@ -6,6 +6,7 @@ import os
 def timescanplot(filename, location):
     os.chdir(location)
     datas, mass_values, values = [], [], []
+    
     f = open(filename, "r")
 
     for line in f:
@@ -16,7 +17,6 @@ def timescanplot(filename, location):
                 del datas[-1]
                 break
     no_of_mass = len(datas[0])-2
-
     f.close()
 
     f = open(filename, "r")
@@ -26,16 +26,10 @@ def timescanplot(filename, location):
             tmp.append(":")
             tmp = tmp[0].strip()+tmp[-1]+tmp[1].strip()
             mass_values.append(tmp)
-
-    #mass_values = [i[i].strip() for i in values[i]]
-
-    #for i in range(no_of_mass):
-    #    mass_values.append(values[i+1])
         
     f.close()
 
     d = {}
-
     for i in range(no_of_mass):
         d["mass_#{0}".format(i)] = []
 
@@ -49,6 +43,48 @@ def timescanplot(filename, location):
     for i in range(no_of_mass):
         plt.scatter(time, d['mass_#{}'.format(i)])
         plt.plot(time, d['mass_#{}'.format(i)], label = mass_values[i])
+
+
+    #Error bar calculations:
+    iterations = [int(i.split(":")[-1]) for i in mass_values]
+    start = False
+    no_of_times = 0
+    raw_datas = []
+
+    f = open(filename, "r")
+    for line in f:
+        if not line[0]=="#" and not line == "\n":
+            line.strip()
+            tmp = line.split()
+            if tmp[0]=="ALL:":
+                start = True
+                continue
+            if start:
+                no_of_times += 1
+                raw_datas.append(line.strip())
+            total = int(no_of_times/sum(iterations))    
+    f.close()
+
+    values = []
+    for datas in raw_datas:
+        values.append(datas.split())
+    values = [[float(i) for i in values[0]] for j in values]
+
+    all_datas = []
+    for j in range(sum(iterations)):
+        i = j*total
+        all_datas.append(values[i:i+total])
+
+    samples = {}
+    for i in range(no_of_mass):
+        samples["mass_sample_{}".format(i)] = []
+
+    k,l = 0, -1
+    for i in iterations:
+        l += 1
+        for j in range(i):
+            samples["mass_sample_{}".format(l)].append(all_datas[k])
+            k += 1
 
     plt.grid(True)
     plt.xlabel("Time (ms)")
