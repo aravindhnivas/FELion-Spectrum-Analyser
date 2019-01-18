76f01135a6a65a1dc027d459519044463a85ca1f
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-01-18 14:53:39 +0100

    updates

--- modules/FELion_definitions.py
+++ modules/FELion_definitions.py
@@ -28,15 +28,42 @@ def ShowInfo(info, msg):
 # Tkinter functions:
 ####################################
 
-#browse files and display it's location and file selected
-width, height = 50, 100
+#labels and enty:
+class my_label(Frame):
+    def __init__(self, parent, label):
+        Frame.__init__(self, parent)
 
-class tkinter_func(Frame):
-    def __init__(self, txt):
-        Frame.__init__(self, bg="sea green")
-        self.txt = txt
+        self.label = Label(self, text=label, font = ("Times", 12, "bold"))
+        self.label.pack()
 
-    def labels(self):
-        label = Label(self, text=self.txt, \
-                font=12, bg="sea green", bd = 1, relief = SOLID)
-        label.place(relx = 0, rely = 0, relwidth = 1)
+class my_entry(Frame):
+    def __init__(self, parent, entry):
+        Frame.__init__(self, parent)
+
+        self.entry = entry
+
+        if self.entry is int(): self.value = IntVar()
+        else: self.value = StringVar()
+        
+        self.value.set(entry)       
+        self.entry = Entry(self, textvariable = self.value, bg = "white",\
+                              bd = 5, font = ("Times", 12, "italic"))
+
+        self.entry.pack()
+
+#labels and entry combined:
+def labels_and_entry(self, *args, **kwargs):
+    label, entry = [i for i in args]
+    
+    v = {'lx': float(), 'ly': float(), 'ex': float(), 'ey':float(), 'l_rh': 0.05, 'l_rw': 0.1,
+                 'e_rw': 0.1, 'e_rh': 0.05}
+    
+    for i in kwargs:
+        if i in v:
+            v[i] = kwargs[i]
+
+    self.l1 = my_label(self, label)
+    self.l1.place(relx = v['lx'], rely = v['ly'], relheight = v['l_rh'], relwidth = v['l_rw'])
+
+    self.e1 = my_entry(self, entry)
+    self.e1.place(relx = v['ex'], rely = v['ey'], relheight = v['e_rh'], relwidth = v['e_rw'])
--- modules/FELion_normline.py
+++ modules/FELion_normline.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 import numpy as np
 import pylab as P
 import sys
