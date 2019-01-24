489cd88a54aa3e7defed072f765a3e21992e2860
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-01-24 19:18:22 +0100

    update

--- modules/Baseline.bat
+++ modules/Baseline.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set file=C:\FELion-GUI\software
-IF EXIST C:\FELion_update_cache del C:\FELion_update_cache
+::IF EXIST C:\FELion_update_cache del C:\FELion_update_cache
 call activate base
 call python %file%\FELion_GUI_Baseline_v2.py
 
