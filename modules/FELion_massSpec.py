2c4bb5019cd0c095d18db1dbb2213a0699e50ce6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-03-07 16:08:01 +0100

    Update FELion_massSpec.py

--- modules/FELion_massSpec.py
+++ modules/FELion_massSpec.py
@@ -26,6 +26,12 @@ def massSpec(fname, mname, temp, bwidth, ie, location,\
         def saveinfo(name):
             if os.path.isfile(my_path+"/MassSpec_DATA/{}.png".format(name)):
                 ShowInfo("SAVED", "File %s.png saved \nin MassSpec_DATA directory"%name)
+        
+        def y_format(label):
+                y_fmt = {'linear': 'linear', 'log':'log'}
+                fmt = y_fmt[label]
+                ax.set_yscale(fmt)
+                plt.draw()
 
         if not combine:
             
@@ -53,7 +59,7 @@ def massSpec(fname, mname, temp, bwidth, ie, location,\
             m_res, m_b0, trap = round(var['res']), int(var['b0']/1000), int(var['trap_time']/1000)
             print(var)
 
-            fig, ax = plt.subplots(1)
+            fig, ax = plt.subplots()
 
             plt.grid(True)
             ax.plot(x, y, label = '%s: res: %.1f; trap:%ims'%(filename.split('.')[0], m_res, trap))
@@ -74,13 +80,6 @@ def massSpec(fname, mname, temp, bwidth, ie, location,\
             axcolor = 'lightgoldenrodyellow'
             rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
             radio = RadioButtons(rax, ('linear', 'log'))
-
-            def y_format(label):
-                y_fmt = {'linear': 'linear', 'log':'log'}
-                fmt = y_fmt[label]
-                ax.set_yscale(fmt)
-                plt.draw()
-                
             radio.on_clicked(y_format)
 
             if save_fig:
@@ -101,12 +100,33 @@ def massSpec(fname, mname, temp, bwidth, ie, location,\
                 mass = np.genfromtxt(file)
                 x, y = mass[:,0], mass[:,1]
 
-                m_res = mass_resolution(file)
+                var = {'trap_time': 'm04_ao04_sa_delay', 'res':'m03_ao13_reso', 'q2_float':'m04_ao09_qd2_float', 'b0':'m03_ao09_width'}
+                print(var)
+
+                with open(file, 'r') as f:
+                    f = np.array(f.readlines())
+                for i in f:
+                    if not len(i.strip())==0 and i.split()[0]=='#':
+                        for j in var:
+                            if var[j] in i.split():
+                                var[j] = float(i.split()[-3])
+                m_res, m_b0, trap = round(var['res']), int(var['b0']/1000), int(var['trap_time']/1000)
+                print(var)
+
                 
                 plt.grid(True)
-                ax.semilogy(x, y, label = '%s: res: %.1f'%(file.split('.')[0], m_res))
+                ax.plot(x, y, label = '%s: res: %.1f; trap:%ims'%(filename.split('.')[0], m_res, trap))
             
                 plt.legend()
+            
+            # Choosing btwn liner and log scale
+            plt.subplots_adjust(left=0.3)
+
+            axcolor = 'lightgoldenrodyellow'
+            rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
+            radio = RadioButtons(rax, ('linear', 'log'))                
+            radio.on_clicked(y_format)
+
             plt.xlabel('mass [u]')
             plt.ylabel('ion counts /{} ms'.format(bwidth))
             plt.grid(True)
