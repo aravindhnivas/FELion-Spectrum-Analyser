5aaa116bfde2350359ce0138b766fe9cc52368bf
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-03-20 18:14:29 +0100

    Update FELion_definitions.py

--- modules/FELion_definitions.py
+++ modules/FELion_definitions.py
@@ -13,8 +13,9 @@ import numpy as np
 copy = lambda pathdir, x: (shutil.copyfile(join(pathdir, x), join(pathdir,"DATA" ,x)), print("%s copied to DATA folder" %x))
 move = lambda pathdir, x: (shutil.move(join(pathdir, x), join(pathdir,"DATA" ,x)), print("%s moved to DATA folder"%x))
 
-# Tkinter messagebox
+colors = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
 
+# Tkinter messagebox
 def ErrorInfo(error, msg):
     root = Tk()
     root.withdraw()
