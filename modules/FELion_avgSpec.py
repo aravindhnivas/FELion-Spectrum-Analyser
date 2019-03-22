d145861eba02cc74a2b713bcb4dffe927f848b8b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-03-22 12:08:58 +0100

    Update FELion_avgSpec.py

--- modules/FELion_avgSpec.py
+++ modules/FELion_avgSpec.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 
+## MODULES ##
+
+## Data analysis and plotting packages
 import numpy as np
-import pylab as P
-import sys
-from os import path
-from scipy.optimize import leastsq
-
-from FELion_normline import norm_line_felix
-from FELion_normline import felix_binning
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter, NullFormatter, NullLocator
+from matplotlib.ticker import MultipleLocator
 import matplotlib.pyplot as plt
-from FELion_definitions import *
 
-## modules
+## FELion modules
+from FELion_normline import norm_line_felix, felix_binning
+from FELion_definitions import ShowInfo, ErrorInfo, move
+
+## Built-in Modules
 import os
 from os.path import dirname, isdir, isfile
-from tkinter import Tk, messagebox
-from tkfilebrowser import askopenfilenames
-
 
 def export_file(fname, wn, inten):
     f = open(fname.split(".pdf")[0] + '.dat','w')
@@ -27,75 +23,11 @@ def export_file(fname, wn, inten):
         f.write("{:8.3f}\t{:8.2f}\n".format(wn[i], inten[i]))
     f.close()
 
-def main(**kwargs):
-    t="Title"
-    ts=10
-    lgs=5
-    minor=5
-    major=50
-    majorTickSize=8
-    xmin=1000
-    xmax=2000
-
-    fig = plt.subplot(1,1,1)
-    plt.rcParams['figure.figsize'] = [6,4]
-    plt.rcParams['figure.dpi'] = 80
-    plt.rcParams['savefig.dpi'] = 100
-    plt.rcParams['font.size'] = ts # Title Size
-    plt.rcParams['legend.fontsize'] = lgs # Legend Size
-
-    my_path = os.getcwd() # getting current directory
-    pwd = os.listdir(my_path + "/DATA") # going into the data folder to fetch all the available data filename.
-    fileNameList = [] # creating a varaiable list : Don't add any data here. You can use the script as it is since it automatically takes the data in the DATA folder
-    for p in pwd:
-        if p.endswith(".felix"): # finding the files only with .felix extension
-            filename = os.path.basename(p) # getting the name of the file
-            file = os.path.splitext(filename)[0] # printing only the file name without the extension .felix
-            fileNameList.append([file]) # saving all the file names in the variable list fileNameList
-        else:
-            continue
-            
-    xs = np.array([],dtype='double')
-    ys = np.array([],dtype='double')
-
-    for l in fileNameList:
-        a,b = norm_line_felix(l[0])
-        fig.plot(a, b, ls='', marker='o', ms=1, label=l[0])
-        xs = np.append(xs,a)
-        ys = np.append(ys,b)
-    fig.legend(title=t) #Set the fontsize for each label
-
-    #Binning
-    binns, inten = felix_binning(xs, ys, delta=DELTA)
-    fig.plot(binns, inten, ls='-', marker='', c='k')
-
-    #Exporting the Binned file.
-    F = 'OUT/average_Spectrum.pdf'
-    export_file(F, binns, inten)
-
-    #Set the Xlim values and fontsizes.
-    fig.set_xlim([xmin,xmax])
-    fig.set_xlabel(r"Calibrated lambda (cm-1)", fontsize=10)
-    fig.set_ylabel(r"Normalized Intensity", fontsize=10)
-    fig.tick_params(axis='both', which='major', labelsize=majorTickSize)
-
-    #Set the Grid value False if you don't need it.
-    fig.grid(True)
-    #Set the no. of Minor and Major ticks.
-    fig.xaxis.set_minor_locator(MultipleLocator(minor))
-    fig.xaxis.set_major_locator(MultipleLocator(major))
-    plt.savefig(F)
-    plt.close()
-    print("Completed.")
-    print()
-
 def avgSpec_plot(*args):
     t, ts, lgs, minor, major, majorTickSize, markersz,\
     xlabelsz, ylabelsz, fwidth, fheight, outFilename,\
     location, mname, temp, bwidth, ie,\
     save, show, DELTA, fileNameList = args
-
-    print(args)
 
     def filesaved():
         if os.path.isfile(my_path+"/OUT/{}.pdf".format(outFilename)) and save:
