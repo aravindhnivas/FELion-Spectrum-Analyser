c0f99a2347d430a964eba8b3cedc8e05697df83f
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-04-05 20:24:50 +0200

    Update setup.py

--- modules/setup.py
+++ modules/setup.py
@@ -1,4 +1,6 @@
-import cx_Freeze, sys, os
+import cx_Freeze
+import sys
+import os
 
 os.environ['TCL_LIBRARY'] = r'C:\ProgramData\Anaconda3\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\ProgramData\Anaconda3\tcl\tcl8.6'
@@ -10,20 +12,22 @@ if sys.platform == 'win32':
 # configure includes and excludes
 
 add_mods = ['numpy.array', 'numpy.genfromtxt']
-executables = [cx_Freeze.Executable("FELion_GUI_v3.py", base=base, icon="FELion_Icon.ico")]
+executables = [cx_Freeze.Executable(
+    "FELion_GUI_v3.py", base=base, icon="FELion_Icon.ico")]
 remove_mods = ['matplotlib', 'scipy', 'numpy', 'PyQt5', 'notebook']
-modules = ["FELion_sa", "FELion_power", "FELion_normline", "FELion_avgSpec", "FELion_massSpec", "FELion_definitions", "just_plot", "depletion_plot", "timescan_plot"]
+modules = ["FELion_sa", "FELion_power", "FELion_normline", "FELion_avgSpec",
+           "FELion_massSpec", "FELion_definitions", "just_plot", "depletion_plot", "timescan_plot"]
 
 pack = {
-		"packages":add_mods, 
-		"includes":modules, 
-		"excludes":remove_mods
+    "packages": add_mods,
+    "includes": modules,
+    "excludes": remove_mods
 }
 
 cx_Freeze.setup(
-    name = "FELion_GUI",
-    options = {"build_exe": pack},
-    version = "3.0",
-    description = "FELion Spectrum Analyser",
-    executables = executables
-    )
+    name="FELion_GUI",
+    options={"build_exe": pack},
+    version="3.0",
+    description="FELion Spectrum Analyser",
+    executables=executables
+)
