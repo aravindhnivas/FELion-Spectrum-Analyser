aa90dbada75b4225e7f0eda088e15b04c289b301
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-04-08 09:49:31 +0200

    Making changes in Installation
    Removing the Baseline GUI bat files

--- Archives/Baseline.bat
+++ Archives/Baseline.bat
@@ -1,6 +1,8 @@
 @echo off
 
-set file=C:\FELion-GUI
+set file=C:\FELion-GUI\software
+IF EXIST C:\FELion_update_cache rmdir /Q /S C:\FELion_update_cache
+
 call activate base
 call python %file%\FELion_GUI_Baseline_v2.py
 
--- modules/Normline.bat
+++ modules/Normline.bat
@@ -1,6 +1,5 @@
 @echo off
 
-
 set file=C:\FELion-GUI\software
 IF EXIST C:\FELion_update_cache rmdir /Q /S C:\FELion_update_cache
 
