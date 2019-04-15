8b2689f90fb35f2d22592d7f00e8a508e1bbc9e3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-04-15 17:58:12 +0200

    Update kinetics.py

--- modules/kinetics.py
+++ modules/kinetics.py
@@ -3,6 +3,7 @@
 # DATA analysis modules
 import numpy as np
 from scipy.optimize import curve_fit
+from uncertainties import unumpy as unp
 
 # Custom Modules
 from timescan_plot import timescanplot
@@ -15,7 +16,7 @@ from time import time as check_time
 from tkinter import Toplevel, ttk, BooleanVar
 
 # FELion Modules
-from FELion_definitions import FELion_Toplevel
+from FELion_definitions import FELion_Toplevel, FELion_widgets
 
 ####################################### Modules Imported #######################################
 
@@ -41,10 +42,70 @@ def kinetics(scanfile, location, dpi, parent):
     fig, canvas = tk_widget.figure(dpi, figsize=(15,5))
     ax = fig.add_subplot(111)
 
-
     frame = tk_widget.get_widget_frame()
+    
+    widget = FELion_widgets(frame)
 
-    tk_widget.check_button_maker(masslist, x = 0.1, y = 0.5)
+    log = widget.entries('Check', 'Log', 0.1, 0.3, relwidth = 0.5, relheight = 0.05, default = False)
+
+    widget.labels('H2: ', 0.1, 0.4, relwidth = 0.2, relheight = 0.05, bd = 2)
+    reactant = widget.entries('Entry', '# density', 0.4, 0.4, relwidth = 0.4, relheight = 0.05, bd = 5)
+
+    widget.labels('He: ', 0.1, 0.5, relwidth = 0.2, relheight = 0.05, bd = 2)
+    He = widget.entries('Entry', '# density', 0.4, 0.5, relwidth = 0.4, relheight = 0.05, bd = 5)
+    
+    tk_widget.check_button_maker(masslist, x = 0.1, y = 0.7)
+    
+    ####################################### PLOTTING DETAILS #######################################
+
+    def plot():
+        mass_check = tk_widget.get_check_values()
+
+        temp_mean, temp_error = [], []
+        counter = 0
+        for n, i in zip(iterations, masslist):
+
+            if mass_check[i].get():
+                print(f'Plotting Mass: {i}\n')
+                lg = f'{i}[{n}]; B0: {b0}ms; Res: {res}'
+                ax.errorbar(time, counts[f'm{i}'], counts[f'me{i}'], fmt='.-', label = lg)
+
+                temp_mean.append(counts[f'm{i}'])
+                temp_error.append(counts[f'me{i}'])
+
+                counter += 1
+            
+        temp_mean_with_error = unp.uarray(temp_mean, temp_error)
+        temp_sum_mean_with_error = temp_mean_with_error.sum(axis = 0)
+
+        if counter>1: 
+            ax.errorbar(time, unp.nominal_values(temp_sum_mean_with_error), unp.std_devs(temp_sum_mean_with_error), fmt = '--', label = f'SUM TOTAL', color = 'k')
+        
+        if log.get(): 
+            ax.set_yscale('log')
+
+        # figure details      
+        ax.set_title('Time Scan plot for %s'%scanfile)
+        ax.set_xlabel('Time (ms)')
+        ax.set_ylabel('Counts')
+        ax.legend()
+        ax.grid(True)
+
+    def update():
+
+        t0 = check_time()
+        ax.clear()
+
+        plot()
+        canvas.draw()
+
+        t1 = check_time()
+
+        print(f'Redrawn in {(t1-t0)*100:.2f} ms')
+
+    plot()
+
+    widget.buttons('Update', 0.1, 0.6, update, relwidth = 0.5, relheight = 0.05)
 
     ####################################### END Plotting details #######################################
     
