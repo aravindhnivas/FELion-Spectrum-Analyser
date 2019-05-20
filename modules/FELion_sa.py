f3192098123232d77cdff2e20786dd33affe0272
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-05-20 16:28:49 +0200

    traceback error output2

--- modules/FELion_sa.py
+++ modules/FELion_sa.py
@@ -17,6 +17,10 @@ from FELion_baseline_old import felix_read_file
 # Built-In Module
 import os
 from os.path import dirname
+
+# Error traceback
+import traceback
+
 ####################################### Modules Imported #######################################
 
 
@@ -27,44 +31,46 @@ class SpectrumAnalyserCalibrator(object):
         fit can be either linear, or cubic
         """
         data = felix_read_file(felixfile)
-    
-        #Spectrum analyser calibration
-        #Added 6.10.16:
-        #Spectrum analyser is calibrated only above 540 cm-1, because for lower values SA gives bulshit values!
-        #In case, someone does not follow the SA, value of SA < 100 will get also excluded from the fit!!!!
-        sa_x = np.copy(data[0][np.logical_and(data[0]>400, data[2]>100)])
-        sa_y = np.copy(data[2][np.logical_and(data[0]>400, data[2]>100)])
 
+        # Spectrum analyser calibration
+        # Added 6.10.16:
+        # Spectrum analyser is calibrated only above 540 cm-1, because for lower values SA gives bulshit values!
+        # In case, someone does not follow the SA, value of SA < 100 will get also excluded from the fit!!!!
+        sa_x = np.copy(data[0][np.logical_and(data[0] > 400, data[2] > 100)])
+        sa_y = np.copy(data[2][np.logical_and(data[0] > 400, data[2] > 100)])
 
         if fit == 'linear':
             p0 = [1.0, 5.0]
-            fitfunc = lambda p, x: p[0]*x + p[1]
+
+            def fitfunc(p, x): return p[0]*x + p[1]
         elif fit == 'cubic':
             p0 = [.01, .01, 1.0, 0.0]
-            fitfunc = lambda p, x: p[0]*x**3 + p[1]*x**2 + p[2]*x + p[3]
+
+            def fitfunc(p, x): return p[0]*x**3 + p[1]*x**2 + p[2]*x + p[3]
         else:
             print("SA fitting function not defined!!!")
 
-
-        errfunc = lambda p, x, y: (fitfunc(p, x)-y)
+        def errfunc(p, x, y): return (fitfunc(p, x)-y)
 
         p, cov_p, info, mesg, ier \
             = leastsq(errfunc, p0, args=(sa_x, sa_y), full_output=1)
-            
+
         if ier not in [1, 2, 3, 4] or cov_p is None:
             msg = "SA calibration optimal parameters not found: " + mesg
             raise RuntimeError(msg)
         if any(np.diag(cov_p) < 0):
-            raise RuntimeError("Optimal parameters not found: negative variance")
-            
+            raise RuntimeError(
+                "Optimal parameters not found: negative variance")
+
         chisq = np.dot(info["fvec"], info["fvec"])
         dof = len(info["fvec"]) - len(p)
-        sigma = np.array([np.sqrt(cov_p[i,i])*np.sqrt(chisq/dof) for i in range(len(p))])
+        sigma = np.array([np.sqrt(cov_p[i, i])*np.sqrt(chisq/dof)
+                          for i in range(len(p))])
 
         self.p = p
         self.sigma = sigma
         self.f = fitfunc
-        self.data = (data[0][data[2]>100], data[2][data[2]>100])
+        self.data = (data[0][data[2] > 100], data[2][data[2] > 100])
         info = self.getInfo()
         print(info)
 
@@ -73,19 +79,21 @@ class SpectrumAnalyserCalibrator(object):
 
     def getData(self):
         return self.data
-    
+
     def getInfo(self):
         info = "#Fit SA vs UNDULATOR (polynome p[0]*x**n + p[1]*x**(n-1) + ... + p[n]):\n"
         info += "#p[]     = " + "".join("{:.4e} ".format(i) for i in self.p) + \
-            "\n#sigma[] = " +    "".join("{:.4e} ".format(i) for i in self.sigma) + "\n"
+            "\n#sigma[] = " + "".join("{:.4e} ".format(i)
+                                      for i in self.sigma) + "\n"
         info += "#----------------------------------------\n"
-        return info 
+        return info
 
     def plot(self, ax):
         wn, sa = self.data
-        X = np.arange(wn.min(),wn.max(), 1)
+        X = np.arange(wn.min(), wn.max(), 1)
         ax.plot(wn, sa, ls='', marker='s', ms=3, markeredgecolor='r', c='r')
         ax.plot(X, self.sa_cm(X), ls='-', marker='', c='g')
+
 
 def FELion_Sa(felixfile, location, dpi, parent):
 
@@ -93,39 +101,41 @@ def FELion_Sa(felixfile, location, dpi, parent):
 
     folders = ["DATA", "EXPORT", "OUT"]
     back_dir = dirname(location)
-    
-    if set(folders).issubset(os.listdir(back_dir)): 
+
+    if set(folders).issubset(os.listdir(back_dir)):
         os.chdir(back_dir)
         location = back_dir
-    
-    else: 
+
+    else:
         os.chdir(location)
 
     ####################################### END Initialisation #######################################
-        
-    saCalibrator = SpectrumAnalyserCalibrator(felixfile, fit='cubic')
-    
-    ####################################### Tkinter figure #######################################
 
-    ## Embedding figure to tkinter Toplevel
-    title_name = 'SA'
-    root = Toplevel(parent)
-    tk_widget = FELion_Toplevel(root, title_name, location)
+    try:
 
-    fig, canvas = tk_widget.figure(dpi)
-    ax = fig.add_subplot(111)
+        saCalibrator = SpectrumAnalyserCalibrator(felixfile, fit='cubic')
 
-    ####################################### PLOTTING DETAILS #######################################
-    saCalibrator.plot(ax)
+        ####################################### Tkinter figure #######################################
 
-    ax.set_title(f'Spectrum analyser calibration from {felixfile} file')
-    ax.set_xlabel("wn set (cm-1)")
-    ax.set_ylabel("wn SA (cm-1)")
+        # Embedding figure to tkinter Toplevel
+        title_name = 'SA'
+        root = Toplevel(parent)
+        tk_widget = FELion_Toplevel(root, title_name, location)
 
-    ####################################### END Plotting details #######################################
+        fig, canvas = tk_widget.figure(dpi)
+        ax = fig.add_subplot(111)
 
-    canvas.draw() # drawing in the tkinter canvas: canvas drawing board
-    
-    ####################################### END Tkinter figure #######################################        
-    # except Exception as e:
-    #     ErrorInfo("ERROR", e)
+        ####################################### PLOTTING DETAILS #######################################
+        saCalibrator.plot(ax)
+
+        ax.set_title(f'Spectrum analyser calibration from {felixfile} file')
+        ax.set_xlabel("wn set (cm-1)")
+        ax.set_ylabel("wn SA (cm-1)")
+
+        ####################################### END Plotting details #######################################
+
+        canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
+
+        ####################################### END Tkinter figure #######################################
+    except:
+        ErrorInfo('Error: ', traceback.format_exc())
--- modules/just_plot.py
+++ modules/just_plot.py
@@ -17,6 +17,9 @@ from FELion_definitions import colors, FELion_Toplevel, ErrorInfo
 import os
 from os.path import isfile, dirname
 
+# Error traceback
+import traceback
+
 ####################################### Modules Imported #######################################
 
 
@@ -46,53 +49,57 @@ def power_plot(powerfile, powerfiles, combine, location, dpi, parent):
 
     ################################ PLOTTING DETAILS ########################################
 
-    def powerplot(powerfile):
-        with open(f'./{powerfile}', 'r') as f:
-            for i in f:
-                if i.find('#SHOTS') >= 0:
-                    shots = int(i.strip().split('=')[-1])
-                    break
+    try:
 
-        power_file = genfromtxt(f'./{powerfile}')
-        power_file_extrapolate = interpolate(
-            power_file[:, 0], power_file[:, 1], kind='linear', fill_value='extrapolate')
-        fname = powerfile.split('.')[0]
+        def powerplot(powerfile):
+            with open(f'./{powerfile}', 'r') as f:
+                for i in f:
+                    if i.find('#SHOTS') >= 0:
+                        shots = int(i.strip().split('=')[-1])
+                        break
 
-        if isfile(f'./{fname}.cfelix'):
-            felixfile = f'{fname}.cfelix'
+            power_file = genfromtxt(f'./{powerfile}')
+            power_file_extrapolate = interpolate(
+                power_file[:, 0], power_file[:, 1], kind='linear', fill_value='extrapolate')
+            fname = powerfile.split('.')[0]
+
+            if isfile(f'./{fname}.cfelix'):
+                felixfile = f'{fname}.cfelix'
+            else:
+                felixfile = f'{fname}.felix'
+
+            temp = genfromtxt(f'./{felixfile}')
+            x = temp[:, 0]
+
+            power_extrapolated = power_file_extrapolate(x)
+
+            ax.plot(power_file[:, 0], power_file[:, 1], '.k')
+            ax.plot(x, power_extrapolated, '-', label=f'{powerfile} : {shots}')
+
+            ax.legend()
+            ax.grid(True)
+            ax.set_title('Power change during scan')
+            ax.set_ylabel('Power (mJ)')
+            ax.set_xlabel('Wavenumber $cm^{-1}$')
+
+        if combine:
+            if powerfiles == []:
+                return ErrorInfo('Combine Mode active', 'Please Select some powerfiles to plot')
+            for file in powerfiles:
+                powerplot(file)
+
         else:
-            felixfile = f'{fname}.felix'
+            if powerfile == '':
+                return ErrorInfo('No File', 'Please Select a powerfile to plot')
+            powerplot(powerfile)
 
-        temp = genfromtxt(f'./{felixfile}')
-        x = temp[:, 0]
+        ####################################### END Plotting details #######################################
 
-        power_extrapolated = power_file_extrapolate(x)
+        canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
 
-        ax.plot(power_file[:, 0], power_file[:, 1], '.k')
-        ax.plot(x, power_extrapolated, '-', label=f'{powerfile} : {shots}')
-
-        ax.legend()
-        ax.grid(True)
-        ax.set_title('Power change during scan')
-        ax.set_ylabel('Power (mJ)')
-        ax.set_xlabel('Wavenumber $cm^{-1}$')
-
-    if combine:
-        if powerfiles == []:
-            return ErrorInfo('Combine Mode active', 'Please Select some powerfiles to plot')
-        for file in powerfiles:
-            powerplot(file)
-
-    else:
-        if powerfile == '':
-            return ErrorInfo('No File', 'Please Select a powerfile to plot')
-        powerplot(powerfile)
-
-    ####################################### END Plotting details #######################################
-
-    canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
-
-    ####################################### END Tkinter figure #######################################
+        ####################################### END Tkinter figure #######################################
+    except:
+        ErrorInfo('Error: ', traceback.format_exc())
 
 
 def plot(filename, filelist, combine, location, dpi, parent):
@@ -114,107 +121,115 @@ def plot(filename, filelist, combine, location, dpi, parent):
     ax = fig.add_subplot(111)
 
     ####################################### PLOTTING DETAILS #######################################
-    if combine:
-        for i in filelist:
-            data = genfromtxt(i)
+
+    try:
+        if combine:
+            for i in filelist:
+                data = genfromtxt(i)
+                x, y = data[:, 0], data[:, 1]
+                ax.plot(x, y, label=i)
+        else:
+            data = genfromtxt(filename)
             x, y = data[:, 0], data[:, 1]
-            ax.plot(x, y, label=i)
-    else:
-        data = genfromtxt(filename)
-        x, y = data[:, 0], data[:, 1]
-        ax.plot(x, y, label=filename)
+            ax.plot(x, y, label=filename)
 
-    ax.legend()
-    ax.set_title('Plot XY')
-    ax.set_xlabel("X-Axis")
-    ax.set_ylabel("Y-Axis")
-    ax.grid(True)
+        ax.legend()
+        ax.set_title('Plot XY')
+        ax.set_xlabel("X-Axis")
+        ax.set_ylabel("Y-Axis")
+        ax.grid(True)
 
-    ####################################### END Plotting details #######################################
+        ####################################### END Plotting details #######################################
 
-    canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
+        canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
 
-    ####################################### END Tkinter figure #######################################
+        ####################################### END Tkinter figure #######################################
+    except:
+        ErrorInfo('Error: ', traceback.format_exc())
 
 
 def exp_theory(filelist, location, dpi, original_show, scale, smooth, parent):
 
     ####################################### Initialisation #######################################
+    try:
 
-    os.chdir(location)
-    dat = [i for i in filelist if i.find('.dat') >= 0]
-    tsv = [i for i in filelist if not i.find('.dat') >= 0]
+        os.chdir(location)
+        dat = [i for i in filelist if i.find('.dat') >= 0]
+        tsv = [i for i in filelist if not i.find('.dat') >= 0]
 
-    window_length, polyorder = asarray(smooth.split(','), dtype=int)
-    print(
-        f'\nSavitzky-Golay filter for smoothening of data\nWindow Length --> {window_length}\nPolyorder --> {polyorder}\n')
+        window_length, polyorder = asarray(smooth.split(','), dtype=int)
+        print(
+            f'\nSavitzky-Golay filter for smoothening of data\nWindow Length --> {window_length}\nPolyorder --> {polyorder}\n')
 
-    ####################################### END Initialisation #######################################
+        ####################################### END Initialisation #######################################
 
-    ####################################### Tkinter figure #######################################
+        ####################################### Tkinter figure #######################################
 
-    # Embedding figure to tkinter Toplevel
-    title_name = 'Exp-Theory'
-    root = Toplevel(parent)
-    tk_widget = FELion_Toplevel(root, title_name, location)
+        # Embedding figure to tkinter Toplevel
+        title_name = 'Exp-Theory'
+        root = Toplevel(parent)
+        tk_widget = FELion_Toplevel(root, title_name, location)
 
-    fig, canvas = tk_widget.figure(dpi)
-    ax = fig.add_subplot(111)
+        fig, canvas = tk_widget.figure(dpi)
+        ax = fig.add_subplot(111)
 
-    ####################################### PLOTTING DETAILS #######################################
+        ####################################### PLOTTING DETAILS #######################################
 
-    y_list = []
-    n = 0
-    if len(dat) > 0:
-        for i in dat:
-            if i.find('.dat') >= 0:
+        y_list = []
+        n = 0
+        if len(dat) > 0:
+            for i in dat:
+                if i.find('.dat') >= 0:
+                    data = genfromtxt(i)
+                    x, y = data[:, 0], data[:, 1]
+                    y = y - y.min()
+
+                    # Apply a Savitzky-Golay filter for smoothening of data. "fit(data, window_length, polyorder)"
+                    y1 = y_fit = array(fit(y, window_length, polyorder))
+                    y_fit = y_fit - y_fit.min()
+
+                    y_list.append(y_fit.max())
+
+                    if original_show:       # To compare the original with the smoothened data
+                        ax.plot(x, y, label=f'{i}_Original')
+                        ax.plot(x, y1, label=f'{i}_fit')
+
+                    elif len(tsv) < 1:
+                        ax.plot(x, y_fit, label=i)
+                    else:
+                        ax.plot(x, y_fit, 'k')
+
+            y_list = array(y_list)
+
+        n = 0
+        if len(tsv) > 0:
+            for i in tsv:
                 data = genfromtxt(i)
                 x, y = data[:, 0], data[:, 1]
-                y = y - y.min()
 
-                # Apply a Savitzky-Golay filter for smoothening of data. "fit(data, window_length, polyorder)"
-                y1 = y_fit = array(fit(y, window_length, polyorder))
-                y_fit = y_fit - y_fit.min()
+                x = x * scale
+                if len(dat) > 0:
+                    y = y/y.max()*y_list.max()
 
-                y_list.append(y_fit.max())
+                ax.vlines(x, ymin=0, ymax=y, color=colors[n], lw=2, label=i)
+                n += 1
 
-                if original_show:       # To compare the original with the smoothened data
-                    ax.plot(x, y, label=f'{i}_Original')
-                    ax.plot(x, y1, label=f'{i}_fit')
+        # ax.legend()
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
-                elif len(tsv) < 1:
-                    ax.plot(x, y_fit, label=i)
-                else:
-                    ax.plot(x, y_fit, 'k')
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+        ax.set_title('Experimental vs Theoretical')
+        ax.set_xlabel("Wavenumber(cm-1)")
+        ax.set_ylabel("Nomalised Intensity")
+        ax.grid(True)
 
-        y_list = array(y_list)
+        ####################################### END Plotting details #######################################
 
-    n = 0
-    if len(tsv) > 0:
-        for i in tsv:
-            data = genfromtxt(i)
-            x, y = data[:, 0], data[:, 1]
+        canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
 
-            x = x * scale
-            if len(dat) > 0:
-                y = y/y.max()*y_list.max()
-
-            ax.vlines(x, ymin=0, ymax=y, color=colors[n], lw=2, label=i)
-            n += 1
-
-    # ax.legend()
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-    # Put a legend to the right of the current axis
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.7))
-    ax.set_title('Experimental vs Theoretical')
-    ax.set_xlabel("Wavenumber(cm-1)")
-    ax.set_ylabel("Nomalised Intensity")
-    ax.grid(True)
-
-    ####################################### END Plotting details #######################################
-
-    canvas.draw()  # drawing in the tkinter canvas: canvas drawing board
-
-    ####################################### END Tkinter figure #######################################
+        ####################################### END Tkinter figure #######################################
+    
+    except:
+        ErrorInfo('Error: ', traceback.format_exc())
