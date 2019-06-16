b954a20b31c9a9652686f62b4842c3b4f597f5f7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-06-16 14:29:00 +0200

    deleting temp

--- ElectronJS/FELion_py/Normline.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set file=C:\FELion-GUI\software
-IF EXIST C:\FELion_update_cache rmdir /Q /S C:\FELion_update_cache
-
-call activate base
-call python %file%\FELion_GUI_v3.py
-
-pause
---
+++ ElectronJS/FELion_py/normlineJS.py
@@ -0,0 +1,9 @@
+import sys
+
+params = sys.argv[1]
+def test(x):
+    print(x)
+
+test(params)
+
+sys.stdout.flush()
