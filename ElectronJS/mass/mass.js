84b3497f46f247d1f46f0ceb82bc5e69cc90d849
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-06-18 02:35:21 +0200

    Update mass.js

--- ElectronJS/mass/mass.js
+++ ElectronJS/mass/mass.js
@@ -1,21 +1,9 @@
-(function() {
-    var childProcess = require("child_process");
-    var oldSpawn = childProcess.spawn;
-
-    function mySpawn() {
-        console.log('spawn called');
-        console.log(arguments);
-        var result = oldSpawn.apply(this, arguments);
-        return result;
-    }
-    childProcess.spawn = mySpawn;
-})();
-
 //Importing required modules
 const { remote } = require('electron');
 const dialog = remote.dialog;
 const mainWindow = remote.getCurrentWindow();
 const path = require('path')
+const spawn = require("child_process").spawn;
 
 //Showing opened file label
 let openFilePlace = document.querySelector('#mass-open-alert')
@@ -72,13 +60,11 @@ function openFile(e) {
 }
 
 //python backend
-function masspec(e) {
 
+function masspec(e) {
     console.log(`File: ${filePaths}; ${typeof filePaths}`)
 
-    const spawn = require("child_process").spawn;
-    const py = spawn('python', [path.join(__dirname, "./mass.py"), filePaths[0]]);
-
-    py.stdout.on('data', data => { console.log(data.toString()) })
-    py.on('close', () => { console.log('Done') })
+    const py = spawn('python', [path.join(__dirname, "./mass.py"), filePaths]);
+    py.stdout.on('data', data => { console.log(data.toString()) });
+    py.on('close', () => { console.log('Done') });
 }
