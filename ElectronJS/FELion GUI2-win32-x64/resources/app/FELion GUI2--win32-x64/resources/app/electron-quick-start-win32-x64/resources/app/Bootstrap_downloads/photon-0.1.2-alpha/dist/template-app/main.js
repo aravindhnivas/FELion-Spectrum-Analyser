146c1419cf2177569cafac29289d0dbf4147a787
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-08-07 17:29:44 +0200

    FELion_GUI2_ElectronJS software

---
+++ ElectronJS/FELion GUI2-win32-x64/resources/app/FELion GUI2--win32-x64/resources/app/electron-quick-start-win32-x64/resources/app/Bootstrap_downloads/photon-0.1.2-alpha/dist/template-app/main.js
@@ -0,0 +1,77 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu } = require('electron')
+
+let mainWindow
+
+process.env.NODE_ENV = 'development';
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    })
+    mainWindow.loadFile('index.html')
+    mainWindow.on('closed', () => mainWindow = null)
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
+})
+
+
+// Create menu template
+const mainMenuTemplate = [
+    // Each object is a dropdown
+    {
+        label: 'File',
+        submenu: [{
+            label: 'Quit',
+            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        }]
+    }
+];
+
+// If OSX, add empty object to menu
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [{
+                role: 'reload'
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
